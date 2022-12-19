c89a3ae799d8009a45b2c1fed0824d6c29a9c5d4
Herve_win <rv.monnier@free.fr>
2022-12-19 23:10:46 +0100

    ok

--- static/code.js
+++ static/code.js
@@ -1,21 +1,31 @@
-// let input = document.getElementById("input");
-// input.addEventListener("keypress", analyze);
-
-// // function analyze() { if (input.value == "Santa Claus") { alert("gagné") } }
-
-// function analyze(event) { alert("ok") };
-
-let input = document.querySelector("input");
+const input = document.querySelector("input");
 const image = document.querySelector("img");
+let click_number = 1;
 
 input.addEventListener("keyup", (e) => logKey());
+image.addEventListener("click", clickOnQR);
 
 function logKey(e) {
-  var x = document.getElementById("saisie").value;
+  var x = input.value;
   if (x.toLowerCase() == "santa claus") {
     document.getElementById("lecteur").play();
-    image.src = "static/champagne.jpg";
+    // document.querySelector("label").style.visibility = "hidden";
+    // input.style.display = "none";
+    document.querySelector("span").style.display = "none";
+    document.querySelector("video").style.display = "inline";
+    document.querySelector("video").play();
   } else {
     document.getElementById("lecteur").pause();
+  }
+}
+
+function clickOnQR() {
+  if (click_number >= 3) {
+    image.style.display = "none";
+    // document.querySelector("label").style.visibility = "visible";
+    // input.style.display = "inline";
+    document.querySelector("span").style.display = "inline";
+  } else {
+    click_number++;
   }
 }
